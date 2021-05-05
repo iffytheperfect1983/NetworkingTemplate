286abae372499dcf23f9ebf5ae2047cc15ce7094
Phanit Pollavith <ppollavith@gmail.com>
2021-05-05 12:44:41 -0700

    Initial Commit

---
+++ NetworkingTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkingTemplate
+//
+//  Created by Phanit Pollavith on 5/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
